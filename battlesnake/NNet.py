2f449b6626eb7f8c7b44df9a9cd54caf978541f4
Matthew Paletta <mattpaletta@gmail.com>
2019-03-02 12:15:59 -0800

    updated travis files and requirements

---
+++ battlesnake/NNet.py
@@ -0,0 +1,2 @@
+class NNet(object):
+    pass
--- battlesnake/__main__.py
+++ battlesnake/__main__.py
@@ -1,20 +1,21 @@
 import json
 from random import randint
-
-import tensorflow as tf
 from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/ping')
+
+@app.route('/ping', methods=["POST"])
 def ping():
     return "Hello!"
 
-@app.route('/start')
+
+@app.route('/start', methods=["POST"])
 def start():
     return json.dumps({
-                "color": "#00FF00"
-        })
+        "color": "#00FF00"
+    })
+
 
 @app.route('/move', methods=["POST"])
 def move():
@@ -22,9 +23,15 @@ def move():
 
     print(request.data)
 
+    board = request.data["board"]
+    snakes = request.data["snakes"]
+    you = request.data["snakes"]["you"]
+    game_id = request.data["game"]["id"]
+    game_turn = request.data["game"]["turn"]
+
     return json.dumps({
-            # Start by picking a random move
-            "move": move[randint(len(move))]
+        # Start by picking a random move
+        "move": move[randint(a = 0, b = len(move) - 1)]
     })
 
 
